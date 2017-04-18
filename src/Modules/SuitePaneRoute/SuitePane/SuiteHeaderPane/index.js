e483b4acba9d8d7eff6b403aedd445eb45533ae1
Carlos Alberto Pérez Dávila <perez.carlos@redbooth.com>
2017-04-18 15:42:11 +0200

    Added rebuild for builds launched

--- src/Modules/SuitePaneRoute/SuitePane/SuiteHeaderPane/index.js
+++ src/Modules/SuitePaneRoute/SuitePane/SuiteHeaderPane/index.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import jenkins from './../../../../jenkins';
 
 
 const SuiteHeaderPane = ({additionalInfo, status_image}) => {
+
+    const onRebuild = () => {
+        return (
+            jenkins.rebuild(additionalInfo, (error, response) => {
+                if (error) {
+                    console.log("There is an error", error)
+                }
+                else {
+                    console.log("The response is", response)
+                }
+            })
+        )
+    };
+
+    const renderRebuild = () => {
+      return (
+          <div className="rebuild-pane">
+              <button className="btn btn-default" onClick={onRebuild}>
+                  Rebuild
+              </button>
+          </div>
+      )
+    };
+
     if (!additionalInfo){
         return(
             <div className="suite-header panel-title">
@@ -10,10 +35,13 @@ const SuiteHeaderPane = ({additionalInfo, status_image}) => {
         )
     } else {
         return(
-            <div className="suite-header panel-title">
-                <img className="status-image" src={status_image} alt="" />
-                <span className="suite-title h1">{additionalInfo.suite || "ERROR"}</span>
-                <span className="build-number h4">{`\t#${additionalInfo.build}`}</span>
+            <div>
+                <div className="suite-header panel-title">
+                    <img className="status-image" src={status_image} alt="" />
+                    <span className="suite-title h1">{additionalInfo.suite || "ERROR"}</span>
+                    <span className="build-number h4">{`\t#${additionalInfo.build}`}</span>
+                </div>
+                {renderRebuild()}
             </div>
         )
     }
--- src/jenkins.js
+++ src/jenkins.js
@@ -2,14 +2,22 @@ import jsonp from 'jsonp'
 
 var Jenkins = class {
     constructor(jobName = null) {
-        this.jobName = jobName || 'qa-test-job';
+        this.defaultJobName = 'qa-test-job';
     }
 
     send = function(params, callback) {
-        const host = `https://server2.qa.redbooth.com:8446/job/${this.jobName}/buildWithParameters?`;
+        const host = `https://server2.qa.redbooth.com:8446/job/${this.defaultJobName}/buildWithParameters?`;
         const parameters = `branch=${params.branch}&environment=${params.environment}&features=${params.features}`;
 
-        jsonp(`${host}${parameters}`, {timeout: 15}, callback)
+        jsonp(`${host}${parameters}`, {timeout: 15}, function (error, response) {
+            callback(error, response)
+        })
+    };
+
+    rebuild = function(params, callback) {
+        const url = `https://server2.qa.redbooth.com:8446/job/${params.suite}/${params.build}/retry`;
+
+        jsonp(`${url}`, {timeout: 15}, function (error, response) {callback(error, response)})
     }
 };
 
