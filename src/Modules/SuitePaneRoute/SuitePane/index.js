1e1ada40c5a4b4e9081e2be6ee18cae78d028c1f
perezcarlos <perez.carlos@redbooth.com>
2017-06-01 09:58:23 +0200

    Revert "Revert "Improvement/jenkins response""

--- src/Modules/SuitePaneRoute/SuitePane/index.js
+++ src/Modules/SuitePaneRoute/SuitePane/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import groupBy from 'lodash.groupby';
 import ExecutionsList from './ExecutionsListPane/index';
 import AdditionalInfo from './AdditionalInfoPane/index';
@@ -6,78 +6,155 @@ import SuiteHeader from './SuiteHeaderPane/index';
 import FilterSelector from './FilterSelectorPane/index';
 import passed from '../../../Images/passed.png'
 import failed from '../../../Images/failed.png'
-import warning from '../../../Images/warning.png'
 import pending from '../../../Images/pending.gif'
+import warning from '../../../Images/warning.png'
+import aborted from '../../../Images/aborted.png'
 
-const SuitePane = ({suite, additionalInfo, onFilterChange, filter, onValidate}) => {
+class SuitePane extends Component {
+    constructor (props) {
+        super (props);
 
-    var statuses = [];
-    var failedSpecs = '';
+        this.state = {
+            statuses: [],
+            failedSpecs: '',
+            statusImage: null,
+            loadState: 'pending'
+        };
 
-    if(suite) {
-        Object.values(suite).forEach((spec) => {
-            statuses.push(spec.status)
-        });
-
-        const filtered = groupBy(suite, (x) => x['status']);
-        const specList = [...filtered.failed || [], ...filtered.warning || []];
-
-        failedSpecs = specList.map( (spec) => spec.file_path )
+        this.getLoadStatus = this.getLoadStatus.bind(this);
+        this.getStatusImageByStatuses = this.getStatusImageByStatuses.bind(this);
     }
 
-    var status_image = null;
-
-    if (statuses.some((status) => { return status === 'pending'})) {
-        status_image = pending;
-    } else if (statuses.some((status) => {return status === 'failed'})) {
-        status_image = failed;
-    } else if (statuses.some((status) => {return status === 'warning'})) {
-        status_image = warning;
-    } else {
-        status_image = passed;
+    componentDidMount () {
+        this.getLoadStatus()
     }
 
-    if(!suite){
+    componentDidUpdate (prevProps){
+        if (prevProps.suite !== this.props.suite || prevProps.jenkinsInfo !== this.props.jenkinsInfo){
+            this.getLoadStatus()
+        }
+    }
+
+    getStatusImageByStatuses () {
+        var statuses = [];
+        var status_image = '';
+        var failed_specs = [];
+        if (this.props.suite) {
+            Object.values(this.props.suite).forEach((spec) => {
+                statuses.push(spec.status)
+            });
+
+            const filtered = groupBy(this.props.suite, (x) => x['status']);
+            const specList = [...filtered.failed || [], ...filtered.warning || []];
+
+            failed_specs = specList.map((spec) => spec.file_path);
+
+            if (statuses.some((status) => {return status === 'pending'})) {
+                status_image = pending;
+            } else if (statuses.some((status) => {return status === 'failed'})) {
+                status_image = failed;
+            } else if (statuses.some((status) => {return status === 'warning'})) {
+                status_image = warning;
+            } else {
+                status_image = passed
+            }
+
+        }
+
+        this.setState({
+            statuses: statuses,
+            failedSpecs: failed_specs,
+            statusImage: status_image
+        })
+    }
+
+    getLoadStatus () {
+        if (this.props.suite && JSON.stringify(this.props.suite) !== "{}") {
+            this.setState({
+                loadState: 'loaded'
+            });
+            if (this.props.jenkinsInfo && this.props.jenkinsInfo.data) {
+                if (this.props.jenkinsInfo.data.building === true) {
+                    this.setState({
+                        statusImage: pending
+                    })
+                } else if (this.props.jenkinsInfo.data.result === 'ABORTED') {
+                    this.setState({
+                        statusImage: aborted
+                    })
+                } else {
+                    this.getStatusImageByStatuses()
+                }
+            } else {
+                this.getStatusImageByStatuses()
+            }
+        } else if (this.props.jenkinsInfo) {
+            if (this.props.jenkinsInfo.error) {
+                this.setState({
+                    loadState: 'error',
+                    statusImage: failed
+                })
+            } else if (this.props.jenkinsInfo.data.building === true) {
+                this.setState({
+                    loadState: 'loading',
+                    statusImage: pending
+                })
+            } else if (this.props.jenkinsInfo.data.result === 'ABORTED') {
+                this.setState({
+                    loadState: 'aborted',
+                    statusImage: aborted
+                })
+            }
+        } else {
+            this.setState({
+                loadState: 'error'
+            }, this.getStatusImageByStatuses)
+        }
+    }
+
+
+    render () {
+        if (!this.props.locationParams) {
+            return (
+                <div className="suite-pane col-md-12">
+                    <div className="no-suite-loaded">
+                        <h1>No suite has been selected</h1>
+                    </div>
+                </div>
+            )
+        }
+
         return (
             <div className="suite-pane col-md-12">
-                <div className="no-suite-loaded">
-                    <h1>No suite has been selected</h1>
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <SuiteHeader
+                            additionalInfo={this.props.additionalInfo}
+                            statusImage={this.state.statusImage}
+                            loadState={this.state.loadState}
+                            locationParams={this.props.locationParams}
+                            failedSpecs={this.state.failedSpecs}
+                        />
+                    </div>
+                    <div className="panel-body">
+                        <AdditionalInfo additionalInfo={this.props.additionalInfo}/>
+                        <FilterSelector
+                            onChange={this.props.onFilterChange}
+                            filter={this.props.filter}
+                            specs={this.props.suite}
+                        />
+                        <ExecutionsList
+                            specs={this.props.suite}
+                            filter={this.props.filter}
+                            loadState={this.state.loadState}
+                            className="executions"
+                            onValidate={this.props.onValidate}
+                        />
+                    </div>
                 </div>
             </div>
         )
     }
-
-    return (
-        <div className="suite-pane col-md-12">
-            <div className="panel panel-default">
-                <div className="panel-heading">
-                    <SuiteHeader
-                        additionalInfo={additionalInfo}
-                        status_image={status_image}
-                        failedSpecs={failedSpecs}
-                    />
-                </div>
-                <div className="panel-body">
-                    <AdditionalInfo additionalInfo={additionalInfo}/>
-                    <FilterSelector
-                        onChange={onFilterChange}
-                        filter={filter}
-                        specs={suite}
-                    />
-                    <ExecutionsList
-                        specs={suite}
-                        filter={filter}
-                        className="executions"
-                        onValidate={onValidate}
-                    />
-                </div>
-            </div>
-        </div>
-    )
-};
-
-SuitePane.defaultProps = {
-    suite: {}
 };
 
 export default SuitePane;
--- src/Modules/SuiteSelector/index.js
+++ src/Modules/SuiteSelector/index.js
@@ -17,8 +17,17 @@ class SuiteSelectorPane extends Component {
         this.buildOptionClass=this.buildOptionClass.bind(this);
     }
 
+    componentDidUpdate (prevProps) {
+        if (prevProps !== this.props) {
+            this.setState({
+                selectedSuite: this.props.buildSelection.selectedSuite || '',
+                selectedBuild: this.props.buildSelection.selectedBuild || ''
+            })
+        }
+    }
+
     render () {
-        const suite = this.props.suites[this.state.selectedSuite] || {};
+        const suite = this.props.suites[this.state.selectedSuite.replace(/[-\s]/g, "_")] || {};
         const executions = suite || {};
         return (
             <div className="selectors">
@@ -27,7 +36,7 @@ class SuiteSelectorPane extends Component {
                         <h3 className="panel-title">Select a suite: </h3>
                     </div>
                     <div className="panel-body">
-                        {this.renderSuites(Object.keys(this.props.suites))}
+                        {this.renderSuites(this.props.suites)}
                     </div>
                 </div>
 
@@ -44,12 +53,12 @@ class SuiteSelectorPane extends Component {
     }
 
     onSelectSuite (event) {
-        this.setState({selectedSuite: event.target.value, selectedBuild: ''})
+        this.setState({selectedSuite: event.currentTarget.value, selectedBuild: ''})
     }
 
     onSelectBuild (event) {
         this.setState({selectedBuild: event.currentTarget.value}, () => {
-            this.props.onSelect({selectedSuite: this.state.selectedSuite, selectedBuild: this.state.selectedBuild})
+            this.props.onSelect(this.state)
         })
     }
 
@@ -73,15 +82,15 @@ class SuiteSelectorPane extends Component {
         return (
             <div className="suite-selector list-group">
                 {
-                    suites.map((suiteName, index)=>{
+                    Object.keys(suites).map((key, index)=>{
                         return(
                             <button
-                                className={this.suiteOptionClass(suiteName)}
+                                className={this.suiteOptionClass(suites[key].job_name || key)}
                                 key={index}
-                                value={suiteName}
+                                value={suites[key].job_name || key}
                                 onClick={this.onSelectSuite}
                             >
-                                {suiteName.replace(/[_-]/g, " ")}
+                                {key.replace(/[_-]/g, " ")}
                             </button>
                         )
                     })
